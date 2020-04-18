51f47447ef05722ae3b1f5a9b2ce15f54ce3e309
Arthur Cesarino <cesarinoarthur@gmail.com>
2020-04-17 21:53:50 -0300

    remove teste.js

--- Exercises/teste.js
+++
@@ -1,4 +0,0 @@
-const colors  = ["blue","red","white","green", "black", "yellow", "pink", "grey", "orange", 'brown','violet']
-bkcolor = colors[Math.floor(Math.random() * 11)]
-
-console.log(bkcolor)
